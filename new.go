328c4e12f7011a7982cfdf256bf5a5e5414da960
Rodolfo Carvalho <rhcarvalho@gmail.com>
2016-06-09 12:15:53 +0200

    Extract function createDatedPackage

--- new.go
+++ new.go
@@ -9,21 +9,27 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
 
-const usage = `go run new.go "problem_name"`
+const usage = `go run new.go PACKAGE_NAME`
+
+func createDatedPackage(name string) (path string, err error) {
+	date := time.Now().Format("2006-01-02")
+	path = filepath.Join(date, name)
+	return path, os.MkdirAll(path, 0775)
+}
 
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("usage:", usage)
 		os.Exit(1)
 	}
-	date := time.Now().Format("2006-01-02")
-	problemName := strings.Replace(os.Args[1], " ", "_", -1)
-	path := fmt.Sprintf("%s/%s", date, problemName)
-	if err := os.MkdirAll(path, 0775); err != nil {
+	name := strings.Replace(os.Args[1], " ", "_", -1)
+	path, err := createDatedPackage(name)
+	if err != nil {
 		fmt.Println("error:", err)
 		os.Exit(1)
 	}
