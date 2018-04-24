7fe870f43196f07ba1128d1c2d9ab4a4b2e9b437
Jan Houska <jhouska@redhat.com>
2018-04-24 17:17:13 +0200

    trying to make test coverage of http://codingdojo.org/kata/GildedRose/ problem

---
+++ 2018/2018-04-23/GildedRose-Refactoring-Kata/js/spec/gilded_rose_spec.js
@@ -0,0 +1,9 @@
+describe("Gilded Rose", function() {
+
+  it("should foo", function() {
+    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toEqual("fixme");
+  });
+
+});
---
+++ 2018/2018-04-23/GildedRose-Refactoring-Kata/scala/build.sbt
@@ -0,0 +1,7 @@
+name := "GildedRose"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1"
